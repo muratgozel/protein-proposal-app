50bbd9295d439f606ee7cb592c738a7f4201698b
Murat Gözel <murat@gozel.com.tr>
2022-06-14 23:57:05 +0300

    ignore env

--- src/lib/routes/CraftProposal.svelte
+++ src/lib/routes/CraftProposal.svelte
@@ -6,6 +6,6 @@
 <h2>Craft A Proposal</h2>
 <div class="links">
   {#each $toolbox.data as toolboxitem}
-    <a href="/proposal/craft/{toolboxitem.slug}" use:link title="Create {toolboxitem.title} Proposal">{toolboxitem.title}</a>
+    <a href="/proposal/craft/{toolboxitem.slug}" use:link title="Create {toolboxitem.title} Proposal">{toolboxitem.title}</a><br>
   {/each}
 </div>
